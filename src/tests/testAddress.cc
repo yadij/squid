462944368db112da6665e2ff60c912e22426f925
Amos Jeffries <amosjeffries@squid-cache.org>
2018-09-23 01:49:17 +1200

    Tests: add stub file for libsbuf.la
    Removeing stub_SBufDetailsStats entirely.
    
    Unfortunately inline methods cause the new stub to depend on other
    symbols, such as SquidConfig so for now move teh stub_SBuf.cc to
    test-suite/ rather than removal so we do not expand the stubs
    imported into that are.

--- src/tests/testAddress.cc
+++ src/tests/testAddress.cc
@@ -27,8 +27,6 @@
 
 CPPUNIT_TEST_SUITE_REGISTRATION( testIpAddress );
 
-#include "tests/stub_SBuf.cc"
-
 /* so that we don't break POD dependency just for the test */
 struct timeval current_time;
 double current_dtime;
