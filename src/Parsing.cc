9c09484aa490c4b3f94340ee62c28339e97268f5
Amos Jeffries <amosjeffries@squid-cache.org>
2020-03-26 07:09:33 +1300

    Add Cfg::DownsizeValue template for safer down-casting

--- src/Parsing.cc
+++ src/Parsing.cc
@@ -40,38 +40,21 @@ int
 xatoi(const char *token)
 {
     int64_t input = xatoll(token, 10);
-    int ret = (int) input;
-
-    if (input != static_cast<int64_t>(ret))
-        throw Cfg::FatalError(ToSBuf("invalid value: '", token, "' is larger than the type 'int'"));
-
-    return ret;
+    return Cfg::DownsizeValue<int>(token, input);
 }
 
 unsigned int
 xatoui(const char *token, char eov)
 {
     int64_t input = xatoll(token, 10, eov);
-    if (input < 0)
-        throw Cfg::FatalError(ToSBuf("invalid value: '", token, "' cannot be less than 0"));
-
-    unsigned int ret = (unsigned int) input;
-    if (input != static_cast<int64_t>(ret))
-        throw Cfg::FatalError(ToSBuf("invalid value: '", token, "' is larger than the type 'unsigned int'"));
-
-    return ret;
+    return Cfg::DownsizeValue<unsigned int>(token, input);
 }
 
 long
 xatol(const char *token)
 {
     int64_t input = xatoll(token, 10);
-    long ret = (long) input;
-
-    if (input != static_cast<int64_t>(ret))
-        throw Cfg::FatalError(ToSBuf("invalid value: '", token, "' is larger than the type 'long'"));
-
-    return ret;
+    return Cfg::DownsizeValue<long>(token, input);
 }
 
 int64_t
@@ -92,15 +75,8 @@ xatoll(const char *token, int base, char eov)
 unsigned short
 xatos(const char *token)
 {
-    long port = xatol(token);
-
-    if (port < 0)
-        throw Cfg::FatalError(ToSBuf("invalid value: '", token, "' cannot be less than 0"));
-
-    if ((port & ~0xFFFF) != 0)
-        throw Cfg::FatalError(ToSBuf("invalid value: '", token, "' is larger than the type 'short'"));
-
-    return port;
+    long input = xatol(token);
+    return Cfg::DownsizeValue<unsigned short>(token, input);
 }
 
 int64_t
@@ -126,12 +102,7 @@ GetInteger(void)
 
     // The conversion must honor 0 and 0x prefixes, which are important for things like umask
     int64_t ret = xatoll(token, 0);
-
-    int i = (int) ret;
-    if (ret != static_cast<int64_t>(i))
-        throw Cfg::FatalError(ToSBuf("invalid value: '", token, "' is larger than the type 'int'"));
-
-    return i;
+    return Cfg::DownsizeValue<int>(token, ret);
 }
 
 /*
--- src/fs/rock/RockSwapDir.cc
+++ src/fs/rock/RockSwapDir.cc
@@ -414,12 +414,13 @@ Rock::SwapDir::parseTimeOption(char const *option, const char *value, int reconf
 
     Cfg::RequireValue(option, value);
 
-    // TODO: handle time units and detect parsing errors better
-    const int64_t parsedValue = strtoll(value, NULL, 10);
+    // TODO: handle time units
+    const int64_t parsedValue = strtoll(value, nullptr, 10);
     Cfg::RequirePositiveValue(option, parsedValue);
 
-    const time_msec_t newTime = static_cast<time_msec_t>(parsedValue);
-    Cfg::RequirePositiveValue(option, newTime);
+    static_assert(std::numeric_limits<time_msec_t>::max() >= std::numeric_limits<decltype(parsedValue)>::max());
+    static_assert(sizeof(time_msec_t) >= sizeof(decltype(parsedValue)));
+    const auto newTime = static_cast<time_msec_t>(parsedValue);
 
     if (!reconfig)
         *storedTime = newTime;
@@ -453,12 +454,9 @@ Rock::SwapDir::parseRateOption(char const *option, const char *value, int isaRec
 
     Cfg::RequireValue(option, value);
 
-    // TODO: handle time units and detect parsing errors better
-    const int64_t parsedValue = strtoll(value, NULL, 10);
-    Cfg::RequirePositiveValue(option, parsedValue);
-
-    const int newRate = static_cast<int>(parsedValue);
-    Cfg::RequirePositiveValue(option, newRate);
+    // TODO: handle time units
+    const int64_t parsedValue = strtoll(value, nullptr, 10);
+    const int newRate = Cfg::DownsizeValue<int>(value, parsedValue);
 
     if (!isaReconfig)
         *storedRate = newRate;
@@ -491,10 +489,11 @@ Rock::SwapDir::parseSizeOption(char const *option, const char *value, int reconf
 
     Cfg::RequireValue(option, value);
 
-    // TODO: handle size units and detect parsing errors better
-    const uint64_t newSize = strtoll(value, NULL, 10);
+    // TODO: handle size units
+    const uint64_t newSize = strtoll(value, nullptr, 10);
     Cfg::RequirePositiveValue(option, newSize);
 
+    // TODO: check for some reasonable max too (eg. the cache_dir maxSize() or such)
     if (newSize <= sizeof(DbCellHeader))
         throw Cfg::FatalError(ToSBuf("cache_dir ", path, ' ', option, " must exceed ", sizeof(DbCellHeader), "; got: ", newSize));
 
@@ -520,8 +519,7 @@ Rock::SwapDir::dumpSizeOption(StoreEntry * e) const
 void
 Rock::SwapDir::validateOptions()
 {
-    if (slotSize <= 0)
-        fatal("Rock store requires a positive slot-size");
+    assert(slotSize <= 0); // validated by parseSizeOption()
 
     const int64_t maxSizeRoundingWaste = 1024 * 1024; // size is configured in MB
     const int64_t slotSizeRoundingWaste = slotSize;
