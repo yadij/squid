40a9e03fdff03b643c03604ec68172d2300b5a27
Amos Jeffries <amosjeffries@squid-cache.org>
2020-07-23 21:02:36 +1200

    Refactor Ssl::createSslPrivateKey()
    * Use the OpenSSL 1.1+ EVP API for generating RSA keys.
    
    * Make static since this is only used by the gadgets.cc code.

--- src/ssl/gadgets.cc
+++ src/ssl/gadgets.cc
@@ -9,35 +9,28 @@
 #include "squid.h"
 #include "ssl/gadgets.h"
 
-EVP_PKEY * Ssl::createSslPrivateKey()
+static EVP_PKEY *
+CreateRsaPrivateKey()
 {
-    Security::PrivateKeyPointer pkey(EVP_PKEY_new());
-
-    if (!pkey)
-        return NULL;
-
-    BIGNUM_Pointer bn(BN_new());
-    if (!bn)
-        return NULL;
-
-    if (!BN_set_word(bn.get(), RSA_F4))
-        return NULL;
-
-    Ssl::RSA_Pointer rsa(RSA_new());
+    Ssl::EVP_PKEY_CTX_Pointer rsa(EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, nullptr));
     if (!rsa)
-        return NULL;
+        return nullptr;
+
+    if (EVP_PKEY_keygen_init(rsa.get()) <= 0)
+        return nullptr;
 
     int num = 2048; // Maybe use 4096 RSA keys, or better make it configurable?
-    if (!RSA_generate_key_ex(rsa.get(), num, bn.get(), NULL))
-        return NULL;
+    if (EVP_PKEY_CTX_set_rsa_keygen_bits(rsa.get(), num) <= 0)
+        return nullptr;
 
-    if (!rsa)
-        return NULL;
+    /* Generate key */
+    Security::PrivateKeyPointer pkey(EVP_PKEY_new());
+    if (pkey) {
+        auto *foo = pkey.get();
+        if (EVP_PKEY_keygen(rsa.get(), &foo) <= 0)
+            return nullptr;
+    }
 
-    if (!EVP_PKEY_assign_RSA(pkey.get(), (rsa.get())))
-        return NULL;
-
-    rsa.release();
     return pkey.release();
 }
 
@@ -551,7 +544,7 @@ static bool generateFakeSslCertificate(Security::CertPointer & certToStore, Secu
     if (properties.signWithPkey.get())
         pkey.resetAndLock(properties.signWithPkey.get());
     else // if not exist generate one
-        pkey.resetWithoutLocking(Ssl::createSslPrivateKey());
+        pkey.resetWithoutLocking(CreateRsaPrivateKey());
 
     if (!pkey)
         return false;
