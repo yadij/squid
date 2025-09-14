82052f6541bf30c09187862ba0c82a300bfa23c9
Amos Jeffries <amosjeffries@squid-cache.org>
2025-09-15 08:17:44 +1200

    Refactor ACLMaxUserIP::match

--- src/auth/AclMaxUserIp.cc
+++ src/auth/AclMaxUserIp.cc
@@ -80,16 +80,20 @@ ACLMaxUserIP::parse()
 int
 ACLMaxUserIP::match(Auth::UserRequest::Pointer auth_user_request, Ip::Address const &src_addr)
 {
+    assert(auth_user_request);
+    auto user = auth_user_request->user();
+    assert(user);
+
     /*
      * the logic for flush the ip list when the limit is hit vs keep
      * it sorted in most recent access order and just drop the oldest
      * one off is currently undecided (RBC)
      */
 
-    if (authenticateAuthUserRequestIPCount(auth_user_request) <= maximum)
+    if (user->ipcount <= static_cast<size_t>(maximum))
         return 0;
 
-    debugs(28, DBG_IMPORTANT, "aclMatchUserMaxIP: user '" << auth_user_request->username() << "' tries to use too many IP addresses (max " << maximum << " allowed)!");
+    debugs(28, DBG_IMPORTANT, "user '" << auth_user_request->username() << "' tries to use too many IP addresses (max " << maximum << " allowed)!");
 
     /* this is a match */
     if (beStrict) {
@@ -98,15 +102,15 @@ ACLMaxUserIP::match(Auth::UserRequest::Pointer auth_user_request, Ip::Address co
          * the request
          */
         /* remove _this_ ip, as it is the culprit for going over the limit */
-        authenticateAuthUserRequestRemoveIp(auth_user_request, src_addr);
-        debugs(28, 4, "aclMatchUserMaxIP: Denying access in strict mode");
+        user->removeIp(src_addr);
+        debugs(28, 4, "Denying access in strict mode");
     } else {
         /*
          * non-strict - remove some/all of the cached entries
          * ie to allow the user to move machines easily
          */
-        authenticateAuthUserRequestClearIp(auth_user_request);
-        debugs(28, 4, "aclMatchUserMaxIP: Denying access in non-strict mode - flushing the user ip cache");
+        user->clearIp();
+        debugs(28, 4, "Denying access in non-strict mode - flushing the user ip cache");
     }
 
     return 1;
